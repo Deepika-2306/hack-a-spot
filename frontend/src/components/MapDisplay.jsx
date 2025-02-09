9241f5db5332a67d3bda1b1faea7de8755adf4b3
dinesh-pasupuleti <78731527+dinesh-pasupuleti@users.noreply.github.com>
2025-02-09 02:05:44 -0500

    directions using real road route

--- frontend/src/components/MapDisplay.jsx
+++ frontend/src/components/MapDisplay.jsx
@@ -1,10 +1,29 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup, Polyline } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 
 const MapDisplay = ({ userCoords, parkingLots, selectedLot }) => {
+  const [route, setRoute] = useState(null);
   const defaultCenter = userCoords ? [userCoords.lat, userCoords.lng] : [35.7796, -78.6382];
+
+  // Fetch route from OSRM when a parking lot is selected
+  useEffect(() => {
+    if (userCoords && selectedLot) {
+      const url = `https://router.project-osrm.org/route/v1/driving/${userCoords.lng},${userCoords.lat};${selectedLot.longitude},${selectedLot.latitude}?overview=full&geometries=geojson`;
+
+      fetch(url)
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.routes.length > 0) {
+            setRoute(data.routes[0].geometry.coordinates.map(([lng, lat]) => [lat, lng])); // Convert to Leaflet format
+          } else {
+            console.error("No route found.");
+          }
+        })
+        .catch((err) => console.error("Error fetching route:", err));
+    }
+  }, [userCoords, selectedLot]);
 
   return (
     <MapContainer center={defaultCenter} zoom={13} style={{ height: "100vh", width: "100%" }}>
@@ -13,7 +32,7 @@ const MapDisplay = ({ userCoords, parkingLots, selectedLot }) => {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
       />
 
-      {/* Show user's location */}
+      {/* Show user's current location */}
       {userCoords && (
         <Marker
           position={[userCoords.lat, userCoords.lng]}
@@ -44,16 +63,8 @@ const MapDisplay = ({ userCoords, parkingLots, selectedLot }) => {
         </Marker>
       ))}
 
-      {/* Show route from user to selected lot */}
-      {selectedLot && userCoords && (
-        <Polyline
-          positions={[
-            [userCoords.lat, userCoords.lng],
-            [selectedLot.latitude, selectedLot.longitude],
-          ]}
-          color="blue"
-        />
-      )}
+      {/* Draw the actual road route from user to selected parking lot */}
+      {route && <Polyline positions={route} color="blue" weight={5} />}
     </MapContainer>
   );
 };
